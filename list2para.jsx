a69a4112ffd3576ae974e634123c4347e2f8e4a4
Prasanth V <prasanthgopalakrishnan@hotmail.com>
2019-11-25 23:53:29 +0530

    Add files via upload

---
+++ list2para.jsx
@@ -0,0 +1,4 @@
+﻿var doc = app.activeDocument;
+doc.stories.everyItem().convertBulletsAndNumberingToText();
+doc.stories.everyItem().tables.everyItem().convertBulletsAndNumberingToText();
+alert("List2para is applied successfully");
